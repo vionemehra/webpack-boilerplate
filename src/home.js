0c16898701f4564258fae00326a2d2fdbc0428ef
Vivek Mehra <vivek.mehra@sqareyards.co.in>
2022-05-14 11:18:01 +0530

    Delete all

--- src/home.js
+++
@@ -1,9 +0,0 @@
-import generateJoke from './generateJoke'
-import './scss/home.scss'
-import avatar from './assets/avatar.png' 
-
-const avatarImg = document.getElementById('avatarImg');
-avatarImg.src = avatar;
-
-const jokeBtn = document.getElementById('jokeBtn');
-jokeBtn.addEventListener('click', generateJoke)
