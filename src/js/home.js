6ed5700093744361e8d441f16d4c84e06cf7c82a
Vivek Mehra <vivek.mehra@sqareyards.co.in>
2022-05-21 21:03:57 +0530

    update jqueryinhancement

--- src/js/home.js
+++ src/js/home.js
@@ -1,17 +1,10 @@
 import $ from 'jquery';
-import _ from 'lodash';
 
 
 
 import "../scss/home.scss"; 
 import "../assets/home/avatar.png"; 
 import '../assets/home/banner.mp4';
-// [1, 2, 3].map(n => n + 1);
 
 
-console.log(_.join(['Hello', 'webpack'], ' '));
-
-$('.avatar').on('click', function() {
-  alert('vivek');
-})
 
